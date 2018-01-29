5a55939f33aacf9a31427bb79a71cc352c2c5909
nzy <798895949@qq.com>
2018-01-29 15:24:49 +0800

    javagit version3

--- src/cn/edu/hdu/Git/GitTest.java
+++ src/cn/edu/hdu/Git/GitTest.java
@@ -6,5 +6,7 @@ package cn.edu.hdu.Git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello NZY");
+        System.out.println("test");
     }
 }
