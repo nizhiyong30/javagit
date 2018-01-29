982c2c4f1fa01faf75baee8efbe637eac41f852b
nzy <798895949@qq.com>
2018-01-29 15:13:48 +0800

    javagit version1

---
+++ src/cn/edu/hdu/Git/GitTest.java
@@ -0,0 +1,10 @@
+package cn.edu.hdu.Git;
+
+/**
+ * Created by nzy on 2018/1/29.
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
